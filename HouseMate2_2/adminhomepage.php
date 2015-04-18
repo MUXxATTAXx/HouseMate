4b58db0a6b14cb2425d4b91cd92e2622bc287ad6
fermenjivar <fernanndo.menjivar@outlook.com>
2015-04-18 15:39:57 -0600

    Estructura Homepage
    Estructurita basica

--- HouseMate2_2/adminhomepage.php
+++ HouseMate2_2/adminhomepage.php
@@ -3,6 +3,7 @@
     <script type='text/javascript' src='js/jquery-1.11.2.min.js'></script>
     <link href='css/bootstrap.min.css' rel='stylesheet'/>
 	<link href='css/intro.css' rel='stylesheet'/>
+    <link href='css/estilo.css' rel='stylesheet'/>
     <title>Inicio</title>");
 ?>
 </head>
@@ -10,5 +11,15 @@
 <?php
     include("Header/barranav2.php");
 ?>
-
+<div id="homepage">
+    <div id="barrabusqueda">
+        <p class="pD2">Barra de Busqueda</p>
+    </div>
+    <div id="BarraLateral">
+        <p class="pD2">Barra Lateral</p>
+    </div>
+    <div id="contenido">
+        <p class="pD2">Contenido</p>
+    </div>
+</div>
 </body>
