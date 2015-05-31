96ae6fc0e7ea11e7c721899db6861b53e9f1641b
AlexBig <muxxattaxx@hotmail.com>
2015-05-31 12:06:28 -0600

    insertar

--- HouseMate2_2/Call/Funciones/registrar.php
+++ HouseMate2_2/Call/Funciones/registrar.php
@@ -1,42 +1,68 @@
 <?php
-    require("conexion.php");
-	
-	$query = "Select usuario FROM tbusuario WHERE usuario ='$usuario'";
-	$result = mysql_query($query);
-	if(mysql_num_rows($result) > 0)
-	{
-		echo $lang['ErUsuarioYa']."<br>";
-	}
-	$query2 = "Select correo FROM tbusuario WHERE correo ='$correo'";
-	$result2 = mysql_query($query2);
-	if(mysql_num_rows($result2) > 0)
-	{
-		echo $lang['ErCorreoya'];	
-	}
-	else
-	{
-		$cantidad = "Select * FROM tbusuario";
-		$numero = mysql_query($cantidad);
-		$digito = mysql_num_rows($numero);
-		$maximun = $digito;
-		if(!isset($_POST['tiposu']))
-		{
-			$tipo = 4;
-		}
+session_start();
+    $nombre = trim($_POST['nombre']);
+    $apellido = trim($_POST['apellido']);
+    $fechanac = $_POST['fechanac'];
+    $correo = trim($_POST['correo']);
+	$usuario = trim($_POST['user']);
+    $contra1 = trim($_POST['contra']);
+    $contra2 = trim($_POST['contra2']);
+	$tipo = $_POST['tiposu'];
+	if(!empty($nombre) and !empty($apellido) and !empty($fechanac) and !empty($correo) and !empty($usuario) and !empty($contra1) and !empty($contra2) and !empty($tipo)) {
+            ingresar($nombre,$apellido,$fechanac,$correo,$usuario,$contra1,$contra2,$tipo);
+     }
+	 
+	function ingresar($nombre,$apellido,$fechanac,$correo,$usuario,$contra1,$contra2,$tipo) {	
+		$con = mysql_connect('localhost','root', '');
+		mysql_select_db('bdhousemate', $con);
+		mysql_query("Set Names 'utf8'");
 		
-		$consulta = "INSERT INTO tbUsuario VALUES ('$maximun','$nombre','$apellido','$fechanac','$correo','$usuario','$contra1','$tipo',NULL)";
-        if(mysql_query($consulta))
+		if($nombre != "" and $apellido != "" and $fechanac != "" and $correo != "" and $usuario != "" and $contra1 != "" and $contra2 != "" and $tipo != 0)
 		{
-			$query = $_SERVER['PHP_SELF'];
-			$path = pathinfo( $query );
-			$url = $path['basename'];
-			echo "<script> 
-			location.replace('cliente_mantenimiento.php?actualstand=1'); 
-			</script>";
+			if($contra1 == $contra2)
+			{
+				$query = "Select usuario FROM tbusuario WHERE usuario ='$usuario'";
+				$result = mysql_query($query);
+				if(mysql_num_rows($result) > 0)
+				{
+					echo $lang['ErUsuarioYa'];
+				}
+				$query2 = "Select correo FROM tbusuario WHERE correo ='$correo'";
+				$result2 = mysql_query($query2);
+				if(mysql_num_rows($result2) > 0)
+				{
+					echo $lang['ErCorreoya'];	
+				}
+				else
+				{
+					$cantidad = "Select * FROM tbusuario";
+					$numero = mysql_query($cantidad);
+					$digito = mysql_num_rows($numero);
+					$maximun = $digito;
+					if(!isset($tipo))
+					{
+						$tipo = 4;
+					}
+					$consulta = "INSERT INTO tbUsuario VALUES ('$maximun','$nombre','$apellido','$fechanac','$correo','$usuario','$contra1','$tipo',NULL)";
+					if(mysql_query($consulta))
+					{
+						
+					}
+					else
+					{
+						
+						echo "<p>Consulta de insertar fallo</p>";
+					}
+				}
+			}
+			else
+			{
+				echo "<p>contraseña incorrecta</p>";
+			}
 		}
-        else
+		else
 		{
-            echo "<p>Consulta de insertar fallo</p>";
+		echo '<p>Campos vacios en Registrarse"</p>';
 		}
-	}
+}
 ?>
--- HouseMate2_2/admincliente/crear_cliente.php
+++ HouseMate2_2/admincliente/crear_cliente.php
@@ -4,21 +4,21 @@
 	<div class="row row-centered">
 		<div class="col-sm-6 col-centered">    
 			<label><?php echo($lang['Nombre']); ?>:</label>
-			<input onkeypress="return letras(event)" class="form-control"maxlength="20" type="text" autocomplete="off" name="nombre" placeholder="<?php echo($lang['Nombre']); ?>" />
+			<input onkeypress="return letras(event)" class="form-control"maxlength="20" type="text" autocomplete="off" id="nombre" placeholder="<?php echo($lang['Nombre']); ?>" />
 		</div>
 		<div class="col-sm-6 col-centered">    
 			<label><?php echo($lang['Apellido']); ?>:</label>
-			<input onkeypress="return letras(event)" class="form-control"maxlength="20" type="text" autocomplete="off" name="apellido" placeholder="<?php echo($lang['Apellido']); ?>" />
+			<input onkeypress="return letras(event)" class="form-control"maxlength="20" type="text" autocomplete="off" id="apellido" placeholder="<?php echo($lang['Apellido']); ?>" />
 		</div>
 	</div>
 	<div class="row row-centered">
 		<div class="col-sm-8 col-centered">  
 			<label><?php echo($lang['Usuarioname']);?>:</label>
-			<input class="form-control" maxlength="20" name="user" autocomplete="off" placeholder="<?php echo($lang['Usuarioname']); ?>" />
+			<input class="form-control" maxlength="20" id="user" autocomplete="off" placeholder="<?php echo($lang['Usuarioname']); ?>" />
 		</div>
 		<div class="col-sm-4 col-centered">  
 			<label><?php echo $lang['Tipous'] ?>:</label>
-			<select class="form-control" name="tiposu">
+			<select class="form-control" id="tiposu">
 				<option value="0"><?php echo $lang['Nada'] ?></option>
 				<option value="2"><?php echo $lang['Cliente'] ?> </option>
 				<option value="3"><?php echo $lang['Perito'] ?> </option>
@@ -29,52 +29,68 @@
 	<div class="row row-centered">
 		<div class="col-sm-8 col-centered">   
 			<label><?php echo($lang['Correo']); ?>:</label>
-			<input id="lowerme" class="form-control"maxlength="30" type="email" autocomplete="off" name="correo" placeholder="<?php echo($lang['Correos']); ?>" />
+			<input id="lowerme" class="form-control"maxlength="30" type="email" autocomplete="off" placeholder="<?php echo($lang['Correos']); ?>" />
 		</div>
 		<div class="col-sm-4 col-centered">   
 			<label><?php echo($lang['Fecha-Nac']); ?>:</label>
-			<input class="form-control" type="date" name="fechanac" placeholder="fecha nacimiento" max="1997-01-01"/>
+			<input class="form-control" type="date" id="fechanac" placeholder="fecha nacimiento" max="1997-01-01"/>
 		</div>
 	</div>
 	<div class="row row-centered">
 		<div class="col-sm-6 col-centered">
 			<label><?php echo($lang['Contra']); ?>:</label>
-			<input class="form-control"maxlength="20" type="password" autocomplete="off" name="contra" placeholder="<?php echo($lang['Contra']); ?>" />
+			<input class="form-control"maxlength="20" type="password" autocomplete="off" id="contra" placeholder="<?php echo($lang['Contra']); ?>" />
 		</div>
 		<div class="col-sm-6 col-centered">
 			<label><?php echo($lang['Confirmar']); ?>:</label>
-			<input class="form-control"maxlength="20" type="password" autocomplete="off" name="contra2" placeholder="<?php echo($lang['Confirmar']); ?>" />
+			<input class="form-control"maxlength="20" type="password" autocomplete="off" id="contra2" placeholder="<?php echo($lang['Confirmar']); ?>" />
 		</div>
 	</div>
 	
    <br>
    <div class="col-sm-6 col-centered">
-    <button class="btn btn-primary btn-block" type="submit" name="registrar"><?php echo($lang['Crear-Cuenta']); ?></button>
+    <button class="btn btn-primary btn-block" id="ingresarstuff"><?php echo($lang['Crear-Cuenta']); ?></button>
+	<p id="resultado"></p>
 	</div>
 </form>
 </div>
-<?php
-//Registrar
-if(isset($_POST['registrar'])){
-    $nombre = trim($_POST['nombre']);
-    $apellido = trim($_POST['apellido']);
-    $fechanac = $_POST['fechanac'];
-    $correo = trim($_POST['correo']);
-	$usuario = trim($_POST['user']);
-    $contra1 = trim($_POST['contra']);
-    $contra2 = trim($_POST['contra2']);
-	$tipo = $_POST['tiposu'];
-    if($nombre != "" and $apellido != "" and $fechanac != "" and $correo != "" and $usuario != "" and $contra1 != "" and $contra2 != "" and $tipo != 0){
-        if($contra1 == $contra2){
-            require "Call/Funciones/registrar.php";
-        }
-        else{
-            echo "contraseña incorrecta";
-        }
-    }
-    else{
-        echo ' <script language="javascript">alert("Campos vacios en Registrarse");</script> ';
-    }
-}
-?>
+<script>                                                      
+        //comprobamos si se pulsa una boton
+        $("#ingresarstuff").click(function(){
+                                     
+		  //obtenemos el texto introducido
+		  nombre = $("#nombre").val();
+		  apellido = $("#apellido").val();
+		  fechanac = $("#fechanac").val();      
+		  correo = $("#lowerme").val();      
+		  user = $("#user").val();      
+		  contra = $("#contra").val();  
+		  contra2 = $("#contra2").val(); 
+		  tiposu = $("#tiposu").val();  
+		  //ingresar usuario
+																			  
+		  $.ajax({
+				type: "POST",
+				url: "Call/Funciones/registrar.php",
+				data: "nombre="+nombre+"&apellido="+apellido+"&fechanac="+fechanac+"&correo="+correo+"&user="+user+"&contra="+contra+"&contra2="+contra2+"&tiposu="+tiposu,
+				dataType: "html",
+				beforeSend: function(){
+					  //imagen de carga
+					  $("#resultado").html("<p align='center'><load.info/images/exemples/26.gif'/></p>");
+				},
+				error: function(){
+					  alert("error petición ajax");
+				},
+				success: function(data){                                                    
+					  $("#resultado").empty();
+					  $("#resultado").append(data);
+														 
+						}
+				  });
+																					  
+																			   
+			});
+																	   
+		</script>
+
 
