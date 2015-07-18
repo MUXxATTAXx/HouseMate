d3798d21d7941540fb4b01c4004d31836fd964c1
AlexBig <muxxattaxx@hotmail.com>
2015-07-18 11:16:26 -0600

    s
    s

--- HouseMate2_2/Call/Funciones/valuotabla.php
+++ HouseMate2_2/Call/Funciones/valuotabla.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+include "../Lenguaje/lenguaje.php";
 start(5);
 function start ($id)
 { ?>
@@ -8,6 +10,20 @@ function start ($id)
     {
       ?>
       <tr id="especial<?= $nur ?>" class="hidme">
+      <td><?php
+      switch($nur)
+      {
+        case 0: echo $lang['Terraza'];
+        break;
+        case 1: echo $lang['Piscinas'];
+        break;
+        case 2: echo $lang['Jardines'];
+        break;
+        case 3: echo $lang['Cocheras'];
+        break;
+        case 4: echo $lang['Sotanos'];
+        break;
+      }?></td>
       <td id="Atabla<?= $nur?>" class="progress-bar progress-bar-primary"></td><td class="progress-bar progress-bar-primary" id="Btabla<?= $nur ?>"></td><td class="progress-bar progress-bar-primary" id="Ctabla<?= $nur ?>"></td>
       </tr>
       <?php
